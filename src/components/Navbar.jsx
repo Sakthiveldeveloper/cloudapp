668410146cb63750e012ecab6568e625f5b8eadb
Sakthiveldeveloper <sakthivel24498@gmail.com>
2022-05-10 15:25:12 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
       <div className='px-2 flex justify-between items-center w-full h-full'>
         <div className='flex items-center'>
-          <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>BRAND.</h1>
+          <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>SCloud!</h1>
           <ul className='hidden md:flex'>
           <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
           <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
